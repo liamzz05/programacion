c8a72fb7f7b00e9d2432b3a95efe63c737264afe
Guille <lidiamupa@gmail.com>
2026-04-14 17:59:59 +0200

    a

--- tema8/hoja2/ej4/src/main/java/hoja2tema8/ej1/Ej4.java
+++ tema8/hoja2/ej4/src/main/java/hoja2tema8/ej1/Ej4.java
@@ -15,53 +15,55 @@ import java.util.Scanner;
  */
 public class Ej4 {
 
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         System.out.println("Introduce el nombre del archivo a copiar");
         String nombre1 = new Scanner(System.in).nextLine();
         File archivo1 = new File(nombre1);
+
         if (!archivo1.exists() || !archivo1.isFile()) {
             System.out.println("Error, archivo no encontrado");
             return;
         }
+
         System.out.println("Introduce el nombre del archivo donde copiar");
         String nombre2 = new Scanner(System.in).nextLine();
         File archivo2 = new File(nombre2);
+
+        FileInputStream lector = null;
+        FileOutputStream escritor = null;
 
         try {
             if (!archivo2.exists()) {
                 archivo2.createNewFile();
                 System.out.println("Archivo " + nombre2 + " creado");
             }
-        } catch (IOException a) {
-            System.out.println("Error al crear el archivo");
-        }
 
-        FileInputStream lector = null;
-        FileOutputStream escritor = null;
-        try {
             lector = new FileInputStream(archivo1);
-            escritor = new FileOutputStream(archivo2, true);
-            
+            escritor = new FileOutputStream(archivo2);
+
             int byteLeido;
             while ((byteLeido = lector.read()) != -1) {
-                escritor.write(byteLeido); 
-                System.out.print((char) byteLeido);                 
+                escritor.write(byteLeido);
+                System.out.print((char) byteLeido);
             }
-        } catch (IOException a) {
-            System.out.println("Error copiando");
-        }finally {
+
+        } catch (IOException e) {
+            System.out.println("Error en la operación: " + e.getMessage());
+
+        } finally {
             if (lector != null) {
                 try {
                     lector.close();
                 } catch (IOException e) {
-                    System.out.println("Error al cerrar el archivo: " + e.getMessage());
+                    System.out.println("Error al cerrar lector: " + e.getMessage());
                 }
             }
+
             if (escritor != null) {
                 try {
                     escritor.close();
                 } catch (IOException e) {
-                    System.out.println("Error al cerrar el archivo: " + e.getMessage());
+                    System.out.println("Error al cerrar escritor: " + e.getMessage());
                 }
             }
         }
