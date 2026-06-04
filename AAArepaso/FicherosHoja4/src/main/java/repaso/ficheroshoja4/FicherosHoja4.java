1758c9c69928a9354d35d8b824f0380266557aaf
liamzz05 <lidiamupa@gmail.com>
2026-06-04 02:20:21 +0200

    a

--- AAArepaso/FicherosHoja4/src/main/java/repaso/ficheroshoja4/FicherosHoja4.java
+++ AAArepaso/FicherosHoja4/src/main/java/repaso/ficheroshoja4/FicherosHoja4.java
@@ -3,11 +3,14 @@
  */
 package repaso.ficheroshoja4;
 
+import java.io.EOFException;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -19,27 +22,42 @@ public class FicherosHoja4 {
         ArrayList<Alumno> alumnos = new ArrayList<>();
         ArrayList<Promocionado> pasan = new ArrayList<>();
 
-        File archivo = new File("D:\\Usuarios\\DAM104\\Downloads", "Alumnos.dat");
-        File promocionan = new File("D:\\Usuarios\\DAM104\\Downloads", "Promocionan.dat");
-        try {
-            promocionan.createNewFile();
-            Alumno leido;
-            // leer alumno y comprobar si promociona
-            while ((leido = Alumno.leerAlumno(archivo)) != null) {
-                alumnos.add(leido);
-                if (leido.getNota1() && leido.getNota2() && leido.getNota3()) {
-                    pasan.add(new Promocionado(leido.getMatricula(),leido.getNombre(),leido.getMedia()));
+        File archivo = new File("C:\\Users\\lidia\\Downloads", "Alumnos.dat");
+
+        File promocionan = new File("C:\\Users\\lidia\\Downloads", "Promocionan.dat");
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(archivo))) {
+
+            while (true) {
+                try {
+                    Alumno leido = Alumno.leerAlumno(ois);
+                    alumnos.add(leido);
+                    System.out.println("Leído: " + leido);
+
+                    if (leido.getNota1() && leido.getNota2() && leido.getNota3()) {
+                        pasan.add(new Promocionado(leido.getMatricula(), leido.getNombre(), leido.getMedia()));
+                    }
+                } catch (EOFException e) {
+                    break;
                 }
             }
-            // escribir lista promocionados en nuevo archivo
-            for(Promocionado alumno : pasan){ 
-                Promocionado.escribirAlumno(alumno, promocionan);
+
+        } catch (IOException | ClassNotFoundException ex) {
+            System.out.println("Error al leer Alumnos.dat: " + ex.getMessage());
+        }
+
+        if (!pasan.isEmpty()) {
+            try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(promocionan))) {
+
+                for (Promocionado alumno : pasan) {
+                    Promocionado.escribirAlumno(alumno, oos);
+                }
+                System.out.println("¡Listo! Se han guardado " + pasan.size() + " alumnos en Promocionan.dat");
+
+            } catch (IOException ex) {
+                System.out.println("Error al escribir en Promocionan.dat: " + ex.getMessage());
             }
-            
-        } catch (IOException ex) {
-            System.out.println("Error: " + ex.getMessage());
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(FicherosHoja4.class.getName()).log(Level.SEVERE, null, ex);
+        } else {
+            System.out.println("Ningún alumno cumple las condiciones para promocionar.");
         }
     }
 
