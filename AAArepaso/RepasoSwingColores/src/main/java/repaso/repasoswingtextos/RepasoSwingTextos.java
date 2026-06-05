46d026c16faaa3b16720313a8facc1e1e04d3ae5
Guille <lidiamupa@gmail.com>
2026-06-05 20:08:40 +0200

    ej repaso

---
+++ AAArepaso/RepasoSwingColores/src/main/java/repaso/repasoswingtextos/RepasoSwingTextos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package repaso.repasoswingtextos;
+
+/**
+ *
+ * @author DAM104
+ */
+public class RepasoSwingTextos {
+
+    public static void main(String[] args) {
+        new Pantalla().setVisible(true);
+    }
+}
