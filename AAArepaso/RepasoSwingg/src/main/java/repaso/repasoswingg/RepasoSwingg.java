9ab36a13d8b90fbbc1d5090bf6af359a3279f49b
Guille <lidiamupa@gmail.com>
2026-06-05 19:30:26 +0200

    ej repaso

---
+++ AAArepaso/RepasoSwingg/src/main/java/repaso/repasoswingg/RepasoSwingg.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package repaso.repasoswingg;
+
+/**
+ *
+ * @author DAM104
+ */
+public class RepasoSwingg {
+
+    public static void main(String[] args) {
+        Pantalla pantalla=new Pantalla();
+        pantalla.setVisible(true);
+    }
+}
